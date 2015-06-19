17de07e35f7c90dc79e565df3a5ca53419b612ce
Kasra Rahjerdi <krahjerdi@stackoverflow.com>
2015-06-19 03:26:23 -0400

    First pass

---
+++ HueMusicViz/ISpectrumProvider.cs
@@ -0,0 +1,10 @@
+﻿using CSCore.Utils;
+namespace WinformsVisualization.Visualization
+{
+    public interface ISpectrumProvider
+    {
+        bool GetFftData(float[] fftBuffer, object context);
+        bool GetFftDataComplex(Complex[] fftBuffer, object context);
+        int GetFftBandIndex(float frequency);
+    }
+}
